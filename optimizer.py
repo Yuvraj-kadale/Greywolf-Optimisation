5f0cc96fa9d5193e73e6a5d4bfc9ce8a23d276d1
Yuvraj Kadale <kadaleyuvraj@gmail.com>
2022-03-13 09:52:15 +0530

    fix optimiser.py

--- optimizer.py
+++ optimizer.py
@@ -1,56 +1,72 @@
-
-import GWO as gwo
-import csv
-import numpy
-import time
-def selection(function_scenario,size_value,Iteration):
-   # functiontion_name=function_scenario[0]
-    l_b=function_scenario[0]
-    u_b=function_scenario[1]
-    dimension=function_scenario[2]
-    x=gwo.GWO(l_b,u_b,dimension,size_value,Iteration)
-    return x
-def parameter(a):
-    parameter = {0: [0, 1, 24]}
-    return parameter.get(a,"noting")
-# Select number of repetitions for each experiment. 
-# To obtain meaningful statistical results, usually 30 independent runs 
-# are executed for each algorithm.
-Number_of_Run=1
-# Select general parameters for all optimizers (population size, number of Iteration)
-Population_Size = 7
-Iteration= 7
-#Export results ?
-Export=True
-
-#ExportToFile="YourResultsAreHere.csv"
-#Automaticly generated name by date and time
-ExportToFile="./result/experiment"+time.strftime("%Y-%m-%d-%H-%M-%S")+".csv"
-
-# Check if it works at least once
-Flag=False
-
-# CSV Header for for the cinvergence 
-CnvgHeader=[]
-
-for l in range(0,Iteration):
-	CnvgHeader.append("Iteration"+str(l+1))
-
-for k in range(0, Number_of_Run):
-    function_scenario = parameter(0)
-    x = selection(function_scenario, Population_Size, Iteration)
-    if (Export == True):
-        with open(ExportToFile, 'a', newline='\n') as out:
-            writeration = csv.writeration(out, delimiteration=',')
-            if (Flag == False):  # just one time to write the header of the CSV file
-                header = numpy.concatenate(
-                    [["Optimizer", "objfname", "startTime", "EndTime", "ExecutionTime"], CnvgHeader])
-                writeration.writerationow(header)
-            a = numpy.concatenate([[x.optimizer, x.objfname, x.startTime, x.endTime, x.executionTime], x.convergence])
-            writeration.writerationow(a)
-        out.close()
-    Flag = True  # at least one experiment
-if (Flag==False): # Faild to run at least one experiment
-    print("No Optomizer or Cost functiontion is selected. Check lists of available optimizers and cost function") 
-        
-        
+from GWO import *
+import random
+import math    
+import copy    
+import sys     
+ 
+ 
+print("\nBegin grey wolf optimization on rastrigin function\n")
+dim = 3
+fitness = fitness_rastrigin
+ 
+ 
+print("Goal is to minimize Rastrigin's function in " + str(dim) + " variables")
+print("Function has known min = 0.0 at (", end="")
+for i in range(dim-1):
+  print("0, ", end="")
+print("0)")
+ 
+num_particles = 50
+max_iter = 100
+ 
+print("Setting num_particles = " + str(num_particles))
+print("Setting max_iter    = " + str(max_iter))
+print("\nStarting GWO algorithm\n")
+ 
+ 
+ 
+best_position = gwo(fitness, max_iter, num_particles, dim, -10.0, 10.0)
+ 
+print("\nGWO completed\n")
+print("\nBest solution found:")
+print(["%.6f"%best_position[k] for k in range(dim)])
+err = fitness(best_position)
+print("fitness of best solution = %.6f" % err)
+ 
+print("\nEnd GWO for rastrigin\n")
+ 
+ 
+print()
+print()
+ 
+ 
+# Driver code for Sphere function
+print("\nBegin grey wolf optimization on sphere function\n")
+dim = 3
+fitness = fitness_sphere
+ 
+ 
+print("Goal is to minimize sphere function in " + str(dim) + " variables")
+print("Function has known min = 0.0 at (", end="")
+for i in range(dim-1):
+  print("0, ", end="")
+print("0)")
+ 
+num_particles = 50
+max_iter = 100
+ 
+print("Setting num_particles = " + str(num_particles))
+print("Setting max_iter    = " + str(max_iter))
+print("\nStarting GWO algorithm\n")
+ 
+ 
+ 
+best_position = gwo(fitness, max_iter, num_particles, dim, -10.0, 10.0)
+ 
+print("\nGWO completed\n")
+print("\nBest solution found:")
+print(["%.6f"%best_position[k] for k in range(dim)])
+err = fitness(best_position)
+print("fitness of best solution = %.6f" % err)
+ 
+print("\nEnd GWO for sphere\n")
